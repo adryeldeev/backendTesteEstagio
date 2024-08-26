2312f908baf30fcc557c70611f69f364ce0704f7
adryel martins <adryeldev@hotmail.com>
2024-08-25 22:58:37 -0300

    fazendo crud na lista de tarefas

--- src/controllers/ListaController.js
+++ src/controllers/ListaController.js
@@ -3,10 +3,125 @@ import { PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default {
-    async createList (req,res){
-        const { titulo, descricao} = req.body;
+    async createTask (req,resp){
+        const { titulo, descricao, userId} = req.body;
 
-        let task = await prisma.task.firs
+        try {
+            let task = await prisma.task.findFirst({where:{titulo, userId}})
+            if(task){
+                return resp.json({
+                    error:true,
+                    message:'Erro, titulo já existe!',
 
+                })
+            }
+            task = prisma.task.create({
+                data:{
+                    titulo,
+                    descricao,
+                    userId
+                }
+            })
+            return resp.json({
+                error:false,
+                message:"Tarefa criada com súcesso",
+                task
+                
+            })
+        } catch (error) {
+            return resp.json({ message: error.message })
+            
+        }
+
+    },
+    async listTaskAll (req,resp){
+        const { userId} = req.params
+        try {
+            let task = await prisma.task.findMany({
+                where:{userId}
+            })
+
+            if(!task){
+                return resp.json({
+                    error:true,
+                    message:'Não há tarefas criada'
+                })
+            }
+
+            return  resp.json({
+                error:false,
+                message:'Tarefas listada com sucesso',
+                task
+            })
+        } catch (error) {
+            return resp.json({ message: error.message })
+        }
+    },
+async findListTask (req,resp){
+    const { id} = req.params
+    try {
+        let task = await prisma.task.findUnique({where:{id:parseInt(id)}})
+        if(!task){
+            return resp.json({
+                error:true,
+                message:"Tarefa não encontrada",
+            })
+        }
+        return resp.json({
+            error:false,
+            message:'Tarefa encontrada',
+            task
+        })
+    } catch (error) {
+        return resp.json({ message: error.message })
     }
+},
+async updateTask(req,resp){
+    const { id} = req.params;
+    const{ titulo, descricao} = req.body;
+    try {
+        let task = await prisma.task.findUnique({where:{id:parseInt(id)}})
+        if(!task){
+            return resp.json({
+               error:true,
+               message:'Tarefa não encontrada' 
+            })
+
+        }
+        task = await prisma.task.update({
+            where:{id:parseInt(id)},
+            data:{titulo, descricao}
+        })
+        return resp.json({
+            error:false,
+            message:'Atualizado com sucesso',
+            task
+        })
+    } catch (error) {
+        return resp.json({ message: error.message })
+    }
+},
+async deleteTask(req,resp){
+    const { id} = req.params
+    try {
+        let task = await prisma.task.findUnique({where:{id:parseInt(id)}})
+        if(!task){
+            return resp.json({
+               error:true,
+               message:'Tarefa não encontrada' 
+            })
+
+        }
+        task = await prisma.task.delete({
+            where:{id:parseInt(id)}
+        })
+        return resp.json({
+            error:false,
+            message:'Deletado com sucesso',
+            task
+        })
+    } catch (error) {
+        return resp.json({ message: error.message })
+    }
+}
 }
