323bd646fea332bd89b0ce214fb09921566682a9
adryel martins <adryeldev@hotmail.com>
2024-09-09 15:41:06 -0300

    config database

--- src/database.js
+++ src/database.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = createPool({
-    host: "junction.proxy.rlwy.net",
-    user: "root",
-    password: "rfIpHuDcUxWcMApaDJWBiQUcjtDgftjR",
-    database: "railway",
-    port: 28665,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
